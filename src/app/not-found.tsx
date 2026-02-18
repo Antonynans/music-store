293a8bf2074cd71180f999e964a46b745bb57ea6
antony <antonynans@gmail.com>
2026-02-18 21:55:36 +0100

    feat: add NotFound page and update Success page layout

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import { Layout } from "../components/layout";
+
+const NotFound: NextPage = () => {
+  return (
+    <Layout>
+      <header className="py-16 px-40 text-white bg-black mb-40 flex flex-col justify-center items-center">
+        <h1 className="uppercase text-4xl">Not found</h1>
+        <p className="mt-4 text-center text-[#cfcfcf]">
+          This page does not exist or has been deleted{" "}
+        </p>
+      </header>
+    </Layout>
+  );
+};
+export default NotFound;
