06755bb83bf4d5b122dd548b8bee8bca82c2ed4d
antony <antonynans@gmail.com>
2026-02-18 16:49:16 +0100

    added images

---
+++ src/assets/index.ts
@@ -0,0 +1,32 @@
+export * as cart_images from './cart/index'
+export * as home_desktop_images from './home/desktop'
+export * as home_tablet_images from './home/tablet'
+export * as home_mobile_images from './home/mobile'
+
+export * as product_xx59_headphones_desktop from './product-xx59-headphones/desktop'
+export * as product_xx59_headphones_tablet from './product-xx59-headphones/tablet'
+export * as product_xx59_headphones_mobile from './product-xx59-headphones/mobile'
+
+export * as product_xx99_mark_one_headphones_desktop from './product-xx99-mark-one-headphones/desktop'
+export * as product_xx99_mark_one_headphones_tablet from './product-xx99-mark-one-headphones/tablet'
+export * as product_xx99_mark_one_headphones_mobile from './product-xx99-mark-one-headphones/mobile'
+
+export * as product_xx99_mark_two_headphones_desktop from './product-xx99-mark-two-headphones/desktop'
+export * as product_xx99_mark_two_headphones_tablet from './product-xx99-mark-two-headphones/tablet'
+export * as product_xx99_mark_two_headphones_mobile from './product-xx99-mark-two-headphones/mobile'
+
+export * as product_yx1_earphones_desktop from './product-yx1-earphones/desktop'
+export * as product_yx1_earphones_tablet from './product-yx1-earphones/tablet'
+export * as product_yx1_earphones_mobile from './product-yx1-earphones/mobile'
+
+export * as product_zx7_speaker_desktop from './product-zx7-speaker/desktop'
+export * as product_zx7_speaker_tablet from './product-zx7-speaker/tablet'
+export * as product_zx7_speaker_mobile from './product-zx7-speaker/mobile'
+
+export * as product_zx9_speaker_desktop from './product-zx9-speaker/desktop'
+export * as product_zx9_speaker_tablet from './product-zx9-speaker/tablet'
+export * as product_zx9_speaker_mobile from './product-zx9-speaker/mobile'
+
+export * as shared_assets_desktop from './shared/desktop'
+export * as shared_assets_tablet from './shared/tablet'
+export * as shared_assets_mobile from './shared/mobile'
