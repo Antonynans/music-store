b34eb7a390dbfa587a11251b56af55d8e91ae54f
antony <antonynans@gmail.com>
2026-02-18 16:45:14 +0100

    installed packages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        "3xl": "0 3px 10px #8b8eaf",
+        "2xs": "rgb(54 54 54) 0px 0px 0px 2px",
+      },
+    },
+  },
+  plugins: [],
+};
